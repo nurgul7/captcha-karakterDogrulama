8edaa8e1b598abf060b2de9214f8fc651e5adac2
nurgül atılmış <84968148+nurgul7@users.noreply.github.com>
2023-09-22 15:27:54 +0300

    Add files via upload

---
+++ Captcha(karakterDogrulama)/Captcha(karakterDogrulama)/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Captcha_karakterDogrulama_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(67, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "KOD İSTE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(123, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(77, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(91, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "KODU GİRİNİZ";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(77, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "DOĞRULA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(320, 303);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "character verification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Captcha(karakterDogrulama)/Captcha(karakterDogrulama)/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captcha_karakterDogrulama_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //KOD İTE butonuna basıldığında
+        {
+            string[] sembol1 = { "j", "ı", "u", "h", "o", "n", "a" };//sembol1 adında bir dizi oluşturduk, küçük harfleri buraya tanımlıyoruz
+            string[] sembol2 = { "+","/","-","#","*"}; //sembol2 diye bir dizi oluşturduk ve buraya , karakterleri tanımlıyoruz
+
+            Random r = new Random(); //Random sınıfından "r" nesnesi oluşturuyoruz
+
+            int s1, s2, s3; //3 tane sayı tanımlıyoruz
+            s1 = r.Next(0, sembol1.Length); //s1'e r nesnesinden 0 ile sembol1 uzunluğu kadar random sayı istiyoruz
+            s2=r.Next(0, sembol2.Length );// buradada 0 ile s2 uzunluğu kadar sayı istiyoruz
+            s3 = r.Next(0, 10);// ve s3 içinde 0 ile 10 arasında bir sayı istiyoruz
+
+            label1.Text = sembol1[s1].ToString() + sembol2[s2].ToString()+ s3.ToString();
+            // label1 de hepsi tanımlayacağımız için + ile bir birine ekleyerek yazdık
+            //sembol1[s1] yazmamızın sebebi= sembol1 dizisinin s1 inci elamanını çağırıyoruz 
+            // s1 zaten bize 7 tane sayı random atacak, atacağı bu sayıları "sembol1[s1]" sembol1 in içine tanımladığımız değerlerden atasın
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e) //DOĞRULA butonuna bastığımızda
+        {
+            if (label1.Text == textBox1.Text )//eğer label1 in değeri textBox1 e eşit ise 
+            {
+                textBox1.BackColor = Color.Green; //textBox ın arka plan rengini yeşil yap
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red; //değil ise arka plan rengini kırmızı yap 
+            }
+        }
+        // ☺
+
+
+    }
+}
